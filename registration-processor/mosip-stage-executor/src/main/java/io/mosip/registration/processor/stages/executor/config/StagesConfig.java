efcc7fe1aca1d994c0b1ea1178ee13fcebf64cf1
ganeshtaru <ganeshtaru91@gmail.com>
2026-01-28 12:30:54 +0530

    WhatsApp Notification

--- registration-processor/mosip-stage-executor/src/main/java/io/mosip/registration/processor/stages/executor/config/StagesConfig.java
+++ registration-processor/mosip-stage-executor/src/main/java/io/mosip/registration/processor/stages/executor/config/StagesConfig.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -36,6 +37,11 @@ import io.vertx.core.json.JsonObject;
 @ImportAutoConfiguration({RefreshAutoConfiguration.class})
 //This is added to fix issue with loading stage specific properties from bootstrap, expected by the stages config.
 @PropertySource("classpath:bootstrap.properties")
+@ComponentScan(basePackages = {
+		"io.mosip.registration.processor.stages.service",
+		"io.mosip.registration.processor.stages.util",
+		"io.mosip.registration.processor.stages.notification"
+})
 public class StagesConfig {
 	
 	private static final String PROP_STAGE_GROUP_NAME = "stage-group-name";
